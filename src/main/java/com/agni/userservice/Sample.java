51832a0d14ad6f105111fcd8b03ec3945dc2a3e3
Shanmugam <datateam1602@gmail.com>
2019-09-19 11:18:53 +0530

    added

---
+++ src/main/java/com/agni/userservice/Sample.java
@@ -0,0 +1,33 @@
+package com.agni.userservice;
+
+public class Sample {
+/*public static <T extends Comparable<T>> int Count(T anArray[],T ele) {
+	
+	int c=0;
+	for(T e:anArray) 
+		if(e.compareTo(ele)>0) 
+			++c;
+			
+		
+	
+	return c;
+	
+}*/
+	
+			public static void main(String args[]){
+		System.out.println("sss");
+		
+		}
+/*			public static void main(int args[]){
+				System.out.println("ssssffff");
+				
+				}
+
+			public static void main(double args[]){
+				System.out.println("ssssffff");
+				
+				}*/
+		   
+		        
+		}
+
